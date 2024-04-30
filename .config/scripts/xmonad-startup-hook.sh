e2a442acab88fe082d2e476a58a2a2567a77ba34
codingfemboy <>
2024-04-30 13:04:57 +1200

    small change

--- .config/scripts/xmonad-startup-hook.sh
+++ .config/scripts/xmonad-startup-hook.sh
@@ -3,7 +3,10 @@
 xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode eDP1 1920x1080
 xrandr --output eDP1 --mode 1920x1080
-source ~/.fehbg
+
+if [ -f ~/.fehbg ]; then
+    source ~/.fehbg
+fi
 
 #xinput set-prop 'SYNA327E:00 06CB:CD4F Touchpad' 'libinput Natural Scrolling Enabled' 1
 #xinput set-prop 'SYNA327E:00 06CB:CD4F Touchpad' 'libinput Tapping Enabled' 1
