8f56aa18c06d3f51abda7a65dca9a1724a100ed9
codingfemboy <>
2024-04-30 12:54:14 +1200

    xmonad config

---
+++ .config/xmonad/xmonad.hs
@@ -0,0 +1,32 @@
+import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+main :: IO()
+main = xmonad
+      . ewmh
+      . withEasySB (statusBarProp "xmobar" (pure def)) defToggleStrutsKey
+      $ myConfig
+
+myKeys conf@(XConfig {modMask = modKey}) = [ ((modKey, xK_f), spawn "firefox") ]
+
+myConfig = def
+  { modMask       = mod4Mask
+  , terminal      = "alacritty"
+  , startupHook   = myStartupHook
+  }
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawn "~/.config/scripts/xmonad-startup-hook.sh"
+
+--main :: IO ()
+--main = xmonad $ def
+--  { modMask = mod4Mask,
+--  terminal = "alacritty",
+--  startupHook = myStartupHook
+--  }
+--  `additionalKeysP` []
