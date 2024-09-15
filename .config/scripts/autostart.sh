657fe920ea8163d77148578855285130666a3c45
codingfemboy <>
2024-09-16 09:51:26 +1200

    a

--- .config/scripts/autostart.sh
+++ .config/scripts/autostart.sh
@@ -4,6 +4,12 @@ xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -
 xrandr --addmode eDP1 1920x1080
 xrandr --output eDP1 --mode 1920x1080
 
+# xrandr --addmode DP1 1920x1080
+# xrandr --output DP1 --mode 1920x1080
+
+# xrandr --addmode DP2 1920x1080
+# xrandr --output DP2 --mode 1920x1080
+
 if [ -e ~/.fehbg ]; then
     source ~/.fehbg
 fi
